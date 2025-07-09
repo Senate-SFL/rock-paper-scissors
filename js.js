41eadc074534ee2c986a728f589bdf81c67c66b6
Senate <aa.gibbins@gmailcom>
2025-07-09 15:50:51 -0500

    Button style game

--- js.js
+++ js.js
@@ -1,66 +1,145 @@
-let humanScore = 0;
-let computerScore = 0;
+const rock = document.getElementById('rock');
+const paper = document.getElementById('paper');
+const scissors = document.getElementById('scissors');
+const p_history = document.getElementById('p-ul');
+const p_score = document.getElementById('p-score');
+const p_score_cont = document.getElementById('score-cont');
+const c_score_cont = document.getElementById('score-cont-c');
+const c_score = document.getElementById('c-score');
+const c_history = document.getElementById('c-ul');
+const c_input = document.getElementById('comp-input');
+const tie = document.getElementById('tie');
+var p_total = 0;
+var c_total = 0;
+
+function disableButton() {
+    rock.disabled = true;
+    paper.disabled = true;
+    scissors.disabled = true;
+}
+
+function checkForTie(player, computer) {
+    if (player === computer) {
+        return true;
+    }
+}
 
 function getComputerChoice() {
     let random = Math.floor(Math.random() * 3);
     if(random == 0) {
-        return "rock";
+        return "Rock";
     } else if(random == 1) {
-        return "paper";
+        return "Paper";
     } else {
-        return "scissors";
+        return "Scissors";
     };
 };
 
-let rpsArray = ['rock', 'paper', 'scissors'];
-
-function getHumanChoice() {
-    let choice = prompt("Rock, paper, or scissors?: ");
-    if (rpsArray.includes(choice.toLowerCase())) {
-        return choice;
+function winRound(playerChoice, computerChoice) {
+    if ((playerChoice === 'Rock' && computerChoice === 'Scissors') || 
+    (playerChoice === 'Paper' && computerChoice === 'Rock') || 
+    (playerChoice === 'Scissors' && computerChoice === 'Paper')) {
+        p_score.textContent = p_total += 1;
+        if (p_total === 5) {
+            p_score_cont.style.backgroundColor = "#00ff0d";
+            disableButton();
+            alert('Player wins!');
+        }
+    } else if (playerChoice === computerChoice) {
+        console.log("tie");
+        tie.textContent = "TIE!";
+        setTimeout(function() {
+            tie.textContent = "";
+        }, 3000);
     } else {
-        console.log("Invalid choice. Please enter: rock, paper, or scissors");
-        getHumanChoice();
-    }
-}
+        c_score.textContent = c_total += 1;
+        if (c_total === 5) {
+            c_score_cont.style.backgroundColor = "#00ff0d";
+            disableButton();
+            alert('Computer wins!');
+        }
+    };
+};
 
 
-function playRound(humanChoice, computerChoice) {
-    if ((humanChoice === 'rock' && computerChoice === 'scissors') || 
-    (humanChoice === 'paper' && computerChoice === 'rock') || 
-    (humanChoice === 'scissors' && computerChoice === 'paper')) {
-        return humanScore += 1;
-    } else if (humanChoice === computerChoice) {
-        return "It's a tie!";
+rock.addEventListener('mouseover', function() {
+    rock.style.backgroundColor = "#999999";
+});
+rock.addEventListener('mouseout', function() {
+    rock.style.backgroundColor = "#eeeeee";
+});
+
+paper.addEventListener('mouseover', function() {
+    paper.style.backgroundColor = "#999999";
+});
+paper.addEventListener('mouseout', function() {
+    paper.style.backgroundColor = "#eeeeee";
+});
+
+scissors.addEventListener('mouseover', function() {
+    scissors.style.backgroundColor = "#999999";
+});
+scissors.addEventListener('mouseout', function() {
+    scissors.style.backgroundColor = "#eeeeee";
+});
+
+rock.addEventListener('click', function() {
+    tie.textContent = "";
+    const newListItem = document.createElement('li');
+    const listItem = document.createElement('li');
+    newListItem.textContent = "Rock";
+    listItem.textContent = getComputerChoice(); //changed
+    winRound('Rock', listItem.innerText);
+    if (checkForTie('Rock', listItem.innerText)) {
+        newListItem.textContent = "Rock (TIE!)";
+        listItem.textContent = "Rock (TIE!)";
+        c_input.textContent = "Rock";
+        p_history.append(newListItem);
+        c_history.append(listItem);
     } else {
-        return computerScore += 1;
+        p_history.append(newListItem);
+        c_history.append(listItem);
+        c_input.textContent = listItem.innerText;
     }
-}
+});
 
-let round = 0;
-
-function playGame() {
-    for(let i = 0; i < 5; i++) {
-        let humanSelection = getHumanChoice();
-        let computerSelection = getComputerChoice();
-        playRound(humanSelection, computerSelection);
-        console.log(`Human: ${humanSelection}`);
-        console.log(`Computer: ${computerSelection}`);
-        round += 1;
-        console.log(`Round: ${round}`);
-        console.log(`Human score: ${humanScore}`);
-        console.log(`Computer score: ${computerScore}`);
-    }
-    if (humanScore === computerScore) {
-        alert('Tie!');
-    } else if (humanScore > computerScore) {
-        alert('Player Wins!');
+paper.addEventListener('click', function() {
+    tie.textContent = "";
+    const newListItem = document.createElement('li');
+    const listItem = document.createElement('li');
+    newListItem.textContent = "Paper";
+    listItem.textContent = getComputerChoice();
+    winRound('Paper', listItem.innerText);
+    if (checkForTie('Paper', listItem.innerText)) {
+        newListItem.textContent = "Paper (TIE!)";
+        listItem.textContent = "Paper (TIE!)";
+        c_input.textContent = "Paper";
+        p_history.append(newListItem);
+        c_history.append(listItem);
     } else {
-        alert('Computer Wins!');
+        p_history.append(newListItem);
+        c_history.append(listItem);
+        c_input.innerText = listItem.innerText;
     }
-}
+});
 
-//Invalid inputs are still being considered part of 
-//the round and disrupts the game round amount
+scissors.addEventListener('click', function() {
+    tie.textContent = "";
+    const newListItem = document.createElement('li');
+    const listItem = document.createElement('li');
+    newListItem.textContent = "Scissors";
+    listItem.textContent = getComputerChoice();
+    winRound('Scissors', listItem.innerText);
+    if (checkForTie('Scissors', listItem.innerText)) {
+        newListItem.textContent = "Scissors (TIE!)";
+        listItem.textContent = "Scissors (TIE!)";
+        c_input.textContent = "Scissors";
+        p_history.append(newListItem);
+        c_history.append(listItem);
+    } else {
+        p_history.append(newListItem);
+        c_history.append(listItem);
+        c_input.innerText = listItem.innerText;
+    }
+});
 
-playGame();
