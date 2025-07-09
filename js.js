e20e6fdf13517e1c0ac23ef807aa4d6169dec00f
Senate <aa.gibbins@gmailcom>
2025-07-09 15:19:27 -0500

    Updated for typing version

--- js.js
+++ js.js
@@ -63,4 +63,12 @@ function playGame() {
 //Invalid inputs are still being considered part of 
 //the round and disrupts the game round amount
 
+if (humanScore === computerScore) {
+    alert("It's a TIE!")
+} else if (humanScore > computerScore) {
+    alert("Player Wins!");
+} else {
+    alert("Computer Wins!");
+}
+
 playGame();
